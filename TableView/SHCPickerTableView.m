e8636365005aa968c1329450d99ed6308507d4dd
Cameron Lowell Palmer <cameron.palmer@shortcut.no>
2013-12-29 17:06:12 +0100

    Beginning to get some movement in the tableview, but the result is far from production.

--- TableView/SHCPickerTableView.m
+++ TableView/SHCPickerTableView.m
@@ -13,12 +13,22 @@
 @interface SHCPickerTableView ()
 @property (strong, nonatomic) UIImageView *selectedImageView;
 @property (strong, nonatomic) CADisplayLink *displayLink;
+@property (assign, nonatomic, getter = isFirstRefresh) BOOL firstRefresh;
 
 @end
 
 
 
 @implementation SHCPickerTableView
+
+- (void)dealloc
+{
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+    
+    [self.displayLink invalidate];
+    [self.displayLink removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    self.displayLink = nil;
+}
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
@@ -32,10 +42,13 @@
 
 - (void)initialize
 {
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+
+    _firstRefresh = YES;
+    
     CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayDidRefresh:)];
-    displayLink.frameInterval = 60.0f;
     [displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
-    self.displayLink = displayLink;
+    _displayLink = displayLink;
 }
 
 
@@ -62,49 +75,82 @@
     }
     
     UITableViewCell *cell = [self cellForRowAtIndexPath:indexPath];
+    if (cell != nil) {
+        UIGraphicsBeginImageContextWithOptions(cell.bounds.size, NO, 0);
+        [cell.layer renderInContext:UIGraphicsGetCurrentContext()];
+        UIImage *cellImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        UIImageView *selectedImageView = [[UIImageView alloc] initWithImage:cellImage];
+        [self addSubview:selectedImageView];
+        CGRect rect = [self rectForRowAtIndexPath:indexPath];
+        selectedImageView.frame = CGRectOffset(selectedImageView.bounds, rect.origin.x, rect.origin.y);
+        
+        selectedImageView.layer.masksToBounds = NO;
+        selectedImageView.layer.shadowColor = [[UIColor blackColor] CGColor];
+        selectedImageView.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
+        selectedImageView.layer.shadowRadius = 6.0f;
+        selectedImageView.layer.shadowOpacity = 0.8f;
+        selectedImageView.layer.opacity = 0.5f;
+        
+        [UIView animateWithDuration:0.25f animations:^{
+            selectedImageView.transform = CGAffineTransformMakeScale(1.1f, 1.1f);
+            selectedImageView.center = CGPointMake(cell.center.x * 1.1f, cell.center.y);
+        }];
+        
+        self.selectedImageView = selectedImageView;
+    }
     
-    UIGraphicsBeginImageContextWithOptions(cell.bounds.size, NO, 0);
-    [cell.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *cellImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+}
+
+- (void)didScrollTableView:(CGFloat)points
+{
+    static CGFloat currentLeftoverPoints = 0.0f;
+    currentLeftoverPoints += points;
     
-    UIImageView *selectedImageView = [[UIImageView alloc] initWithImage:cellImage];
-    [self addSubview:selectedImageView];
-    CGRect rect = [self rectForRowAtIndexPath:indexPath];
-    selectedImageView.frame = CGRectOffset(selectedImageView.bounds, rect.origin.x, rect.origin.y);
+    UITableViewCell *cell = [self cellForRowAtIndexPath:self.selectedIndexPath];
+    CGFloat cellHeight = CGRectGetHeight(cell.frame);
+
+    int numberOfCellsCrossed = floorf(fabsf(currentLeftoverPoints) / cellHeight);
     
-    selectedImageView.layer.masksToBounds = NO;
-    selectedImageView.layer.shadowColor = [[UIColor blackColor] CGColor];
-    selectedImageView.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
-    selectedImageView.layer.shadowRadius = 6.0f;
-    selectedImageView.layer.shadowOpacity = 0.8f;
-    selectedImageView.layer.opacity = 0.5f;
-    
-    [UIView animateWithDuration:0.25f animations:^{
-        selectedImageView.transform = CGAffineTransformMakeScale(1.1f, 1.1f);
-        selectedImageView.center = CGPointMake(cell.center.x * 1.1f, cell.center.y);
-    }];
-    
-    self.selectedImageView = selectedImageView;
+    [self beginUpdates];
+    for (int i = 0; i < numberOfCellsCrossed; i++) {
+        CGFloat distance = cellHeight * numberOfCellsCrossed;
+        CGFloat direction = currentLeftoverPoints < 0.0f ? -1.0f : 1.0f;
+        
+        NSIndexPath *currentIndexPath = self.selectedIndexPath;
+        NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:currentIndexPath.row + numberOfCellsCrossed inSection:currentIndexPath.section];
+        DDLogVerbose(@"MoveRowAtIndexPath:%@ toIndexPath:%@", currentIndexPath, newIndexPath);
+        [self moveRowAtIndexPath:currentIndexPath toIndexPath:newIndexPath];
+        currentLeftoverPoints += direction * distance;
+        
+        [self highlightCellAtIndexPath:newIndexPath];
+        _selectedIndexPath = newIndexPath;
+    }
+    [self endUpdates];
 }
 
 
 
-#pragma mark - UIScrollViewDelegate
+#pragma mark - CADisplayLink callback
 
 - (void)displayDidRefresh:(CADisplayLink *)displayLink
 {
-    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+    static CGFloat previousContentOffsetY;
+    UIEdgeInsets contentInsets = self.contentInset;
+    CGFloat currentContentOffsetY = self.contentOffset.y + contentInsets.top;
     
-    DDLogVerbose(@"%@", displayLink);
- 
-    CGPoint contentOffset = self.contentOffset;
-    contentOffset.y = contentOffset.y + floorf(CGRectGetHeight(self.frame) / 2.0f);
-    NSIndexPath *newIndexPath = [self indexPathForRowAtPoint:self.contentOffset];
-    if (![self.selectedIndexPath isEqual:newIndexPath]) {
-        [self moveRowAtIndexPath:self.selectedIndexPath toIndexPath:newIndexPath];
-        [self highlightCellAtIndexPath:newIndexPath];
-        self.selectedIndexPath = newIndexPath;
+    if (previousContentOffsetY != currentContentOffsetY) {
+        CGFloat points = currentContentOffsetY - previousContentOffsetY;
+        if (!self.isFirstRefresh) {
+            DDLogVerbose(@"offset: %f", points);
+            
+            [self didScrollTableView:points];
+        } else {
+            self.firstRefresh = NO;
+        }
+        
+        previousContentOffsetY = currentContentOffsetY;
     }
  }
 
